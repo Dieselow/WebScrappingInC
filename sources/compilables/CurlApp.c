027eda4416684ac01740c9f89bdddbf233f75dec
Gwen <gwendou95@hotmail.fr>
2019-10-30 10:47:30 +0100

    WIP on curl

---
+++ sources/compilables/CurlApp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "curl.h"
+#include "Application.h"
+
+int main()
+{
+    start();
+ /*
+    CURL *curl = curl_easy_init();
+    if(curl) {
+      CURLcode res;
+      curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+      res = curl_easy_perform(curl);
+      curl_easy_cleanup(curl);
+    }
+*/
+//    printf("Hello world!\n");
+    return 0;
+}
