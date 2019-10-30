cbb9223a5807e212a356a3f47847399b271ccccc
Gwen <gwendou95@hotmail.fr>
2019-10-30 09:33:07 +0100

    Implementation of the Interface

---
+++ sources/compilables/Application.c
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "Application.h"
+#include "Interface.h"
+
+/**
+ * Start the main menu of the software
+ */
+void start(){
+    mainMenu();
+}
+
+/**
+ * Change the color of the font and the background of 
+ * @param t     parameter to change the font color
+ * @param f     parameter to change the background color
+ */
+void color(int t,int f){
+        HANDLE H=GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleTextAttribute(H,f*16+t);
+}
---
+++ sources/headers/Interface.h
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Interface.h
+ * Author: Mikadows
+ *
+ * Created on 14 octobre 2019, 20:26
+ */
+
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    void mainMenu();
+    void printTitle();
+    void printExit();
+    void printMenu(int i);
+    void printSubMenu(int i);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERFACE_H */
+
