57176cb7421590f2238cd08129257fff6d3e6ca4
Gwen <gwendou95@hotmail.fr>
2019-11-03 22:39:05 +0100

    Quick scrapping is done, saving files into ressources/

--- sources/compilables/CurlApp.c
+++ sources/compilables/CurlApp.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "curl.h"
+#include "../headers/Color.h"
+#include "../headers/Application.h"
 
 #define CURL_OK 0
 
@@ -38,6 +40,86 @@ size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s){
   return size*nmemb;
 }
 /**
+ * Remove the headers of the urls
+ * @param url
+ */
+int startSiteName(char* url){
+    int ret = 0;
+
+    if(url[0] == 'h' && url[1] == 't' && url[2] == 't' && url[3] == 'p' &&
+       url[4] == 's' && url[5] == ':' && url[6] == '/' && url[7] == '/'){
+            ret = 8;
+    } else if (url[0] == 'h' && url[1] == 't' && url[2] == 't' && url[3] == 'p' &&
+               url[4] == ':' && url[5] == '/' && url[6] == '/'){
+            ret = 7;
+    }
+    return ret;
+}
+/**
+ *  Create the allpath with a correct file name
+ *  @param url to create the name
+ */
+char* makeFileName(char* url){
+    unsigned int urlSize = strlen(url);
+    char* filename = malloc(sizeof(char) * (urlSize + 20));
+    //char filename[1000];
+    char path[200] = {'r','e','s','s','o','u','r','c','e','s','\\','\0'};
+    char extension[6] = {'.','h','t','m','l','\0'};
+
+    int i = 0;
+    int yFile = 0;
+
+    while(path[i] != '\0'){
+        filename[yFile] = path[i];
+        i++;
+        yFile++;
+    }
+    i = startSiteName(url);
+    while(url[i] != '\0'){
+       if(url[i] == '/' || url[i] == ':'){
+            filename[yFile] = '-';
+        }else{
+            filename[yFile] = url[i];
+        }
+        i++;
+        yFile++;
+    }
+    i = 0;
+    while(i != 6){
+        filename[yFile] = extension[i];
+        i++;
+        yFile++;
+    }
+
+    return filename;
+}
+/**
+ * This function is used to save the content of the site into a file
+ * @param char* str <- content of the site
+ * @param char* url <- url of the site to name the file
+ */
+void saveIntoFile(char* str, char* url){
+    FILE* f;
+    char* filename = makeFileName(url);
+    //printf("\n%s\n", filename);
+
+    f = fopen(filename, "w");
+    if(f != NULL){
+        fprintf(f, str);
+        color(LIGHT_GREEN, BLACK);
+        printf("\n\t\t\t\t\Site Scrapped\n");
+        color(WHITE, BLACK);
+    } else {
+        color(RED, BLACK);
+        printf("\n\t\t\t\t\tFailed to create the file\n");
+        color(WHITE, BLACK);
+    }
+
+    fclose(f);
+
+    free(filename);
+}
+/**
  *  Scrap the URL in argument
  */
 void scrap(char* url) {
@@ -59,88 +141,13 @@ void scrap(char* url) {
 //          printf("%s", s.ptr);
         }else{
             const char*  returnMessage = curl_easy_strerror(res);
+            color(RED, BLACK);
             printf("\n\t\t\t\t\t%s\n\n", returnMessage);
+            color(WHITE, BLACK);
         }
 
         //saveIntoFile(s.ptr, url);
-
         curl_easy_cleanup(curl);    //free of curl
 
     }
-}
-
-/**
- * This function is used to save the content of the site into a file
- * @param char* str <- content of the site
- * @param char* url <- url of the site to name the file
- */
-void saveIntoFile(char* str, char* url){
-    FILE* f;
-    unsigned int urlSize = strlen(url);
-    //char* filename = malloc(sizeof(char) * (urlSize + 5));
-    char filename[1000];
-    char path[200] = {'r','e','s','s','o','u','r','c','e','s','\\','\0'};
-    char extension[6] = {'.','h','t','m','l','\0'};
-
-    int i = 0;
-    int yFile = 0;
-
-    while(path[i] != '\0'){
-//      printf("%c", path[i]);
-        filename[yFile] = path[i];
-        i++;
-        yFile++;
-    }
-
-    i = startSiteName(url);
-
-    while(url[i] != '\0'){
-       if(url[i] == '/'){
-            filename[yFile] = '-';
-        }else{
-            filename[yFile] = url[i];
-        }
-        i++;
-        yFile++;
-    }
-    i = 0;
-    while(i != 6){
-    //while(extension[i] != '\0'){
-//        printf("%c", extension[i]);
-        filename[yFile] = extension[i];
-        i++;
-        yFile++;
-    }
-
-    printf("\n%s\n", filename);
-
-    //ressources\\site.html
-    f = fopen(filename, "w");
-
-    if(f != NULL){
-        fprintf(f, str);
-        printf("File created\n");
-    } else {
-        printf("Failed to create the file\n");
-    }
-
-    fclose(f);
-
-    //free(filename);
-}
-/**
- * Remove the headers of the urls
- * @param url
- */
-int startSiteName(char* url){
-    int ret = 0;
-
-    if(url[0] == 'h' && url[1] == 't' && url[2] == 't' && url[3] == 'p' &&
-       url[4] == 's' && url[5] == ':' && url[6] == '/' && url[7] == '/'){
-            ret = 8;
-    } else if (url[0] == 'h' && url[1] == 't' && url[2] == 't' && url[3] == 'p' &&
-               url[4] == ':' && url[5] == '/' && url[6] == '/'){
-            ret = 7;
-    }
-    return ret;
 }
--- sources/headers/CurlApp.h
+++ sources/headers/CurlApp.h
@@ -9,5 +9,7 @@ void init_string(struct string *s);
 size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s);
 void scrap(char*);
 void saveIntoFile(char* str, char* url);
+int startSiteName(char* url);
+char* makeFileName(char* url);
 
 #endif // CURLAPP_H_INCLUDED
